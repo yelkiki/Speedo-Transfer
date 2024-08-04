4a828ebdb9399d4b1c9614a12fe551816ccd536d
AbdoKhattab <mr.spykhattab@gmail.com>
2024-08-04 22:06:59 +0300

    Added Configrations to Redis

--- src/main/java/com/example/speedotansfer/config/RedisConfig.java
+++ src/main/java/com/example/speedotansfer/config/RedisConfig.java
@@ -19,46 +19,13 @@ import java.time.Duration;
 @Configuration
 public class RedisConfig {
 
-//    @Bean
-//    public RedisCacheConfiguration cacheConfiguration() {
-//        return RedisCacheConfiguration.defaultCacheConfig()
-//                .entryTtl(Duration.ofMinutes(30))
-//                .disableCachingNullValues()
-//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
-//    }
-//
-//    @Bean
-//    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
-//        return (builder) -> builder
-//                .withCacheConfiguration("itemCache",
-//                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(10)))
-//                .withCacheConfiguration("customerCache",
-//                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)));
-//    }
-
-
-//    @Bean
-//    public LettuceConnectionFactory redisStandAloneConnectionFactory() {
-//        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("127.0.0.1", 6379));
-//    }
-
-//    @Bean
-//    JedisConnectionFactory jedisConnectionFactory() {
-//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
-//
-//        return new JedisConnectionFactory(redisStandaloneConfiguration);
-//    }
-
-
-//    @Bean
-//    public JedisConnectionFactory jedisConnectionFactory(){
-//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-//        redisStandaloneConfiguration.setHostName("127.0.0.1");
-//        redisStandaloneConfiguration.setPort(6379);
-//        redisStandaloneConfiguration.setPassword(RedisPassword.of("yourRedisPasswordIfAny"));
-//        return new JedisConnectionFactory(redisStandaloneConfiguration);
-//    }
-
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
 
     @Bean
     public RedisTemplate<String, Long> redisTemplate
